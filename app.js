cf7968cf32d46ac17e8f2e4af81023a424dc3f06
noman-999 <n86462491@gmail.com>
2026-06-12 11:53:34 +0530

    Add first StudentOS interaction

---
+++ app.js
@@ -0,0 +1,21 @@
+document.querySelector("button").addEventListener("click",()=>{
+
+const education=document.getElementById("education").value;
+
+const goal=document.getElementById("goal").value;
+
+alert(
+
+"Welcome to StudentOS!\n\nEducation: "
+
++education+
+
+"\nDream: "
+
++goal+
+
+"\n\nNext version will generate your AI roadmap."
+
+);
+
+});
