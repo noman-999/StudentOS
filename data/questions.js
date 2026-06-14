d378df2a16a8aec828804cc6cd4c6e8a1103e58b
noman-999 <n86462491@gmail.com>
2026-06-14 14:51:55 +0530

    Add StudentOS question data

---
+++ data/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    id: 1,
+    question: "What are you studying?",
+    options: ["Inter", "Degree", "B.Tech", "Other"]
+  },
+  {
+    id: 2,
+    question: "What is your dream career?",
+    options: []
+  },
+  {
+    id: 3,
+    question: "How many hours can you study every day?",
+    options: ["30 mins", "1 hour", "2 hours", "3+ hours"]
+  },
+  {
+    id: 4,
+    question: "What is your biggest confusion?",
+    options: []
+  },
+  {
+    id: 5,
+    question: "Which state are you from?",
+    options: []
+  }
+];
